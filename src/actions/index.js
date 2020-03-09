18ce5fb81c1170fe6616cda075c555a474bb27b5
Gio Justiniano <giojustiniano@Gios-Air.home>
2020-03-09 15:56:28 +0100

    Add actions

---
+++ src/actions/index.js
@@ -0,0 +1,55 @@
+/*
+ * Created on Sun Mar 08 2020
+ * Author: Gio Justiniano
+ * More info: https://github.com/giomikee/
+ *
+ * Copyright (c) 2020
+ */
+
+import { FETCH_DOG_BREEDS, FETCH_DOG_BREED_IMAGES, FETCH_ERROR, GET_TOP_DOGS } from './types';
+
+/* eslint-disable no-magic-numbers */
+
+const API_PREFIX = 'https://dog.ceo/api/breed';
+const ALL_BREEDS = `${API_PREFIX}s/list/all`;
+
+export const fetchDogBreeds = () => dispatch => {
+	fetch(ALL_BREEDS)
+		.then(res => res.json())
+		.then(dogs => dispatch({
+			type: FETCH_DOG_BREEDS,
+			isLoaded: true,
+			payload: Object.keys(dogs.message)
+		}))
+		.catch(error => dispatch({
+			type: FETCH_ERROR,
+			isLoaded: true,
+			payload: error
+		}));
+};
+
+export const fetchDogBreedImages = (dogBreed, isLastBreedLoaded = false) => dispatch => {
+	fetch(`${API_PREFIX}/${dogBreed}/images`)
+		.then(res => res.json())
+		.then(images => dispatch({
+			type: FETCH_DOG_BREED_IMAGES,
+			breed: dogBreed,
+			isLastBreedLoaded,
+			payload: images.message.length
+		}))
+		.catch(error => dispatch({
+			type: FETCH_ERROR,
+			isLoaded: true,
+			payload: error
+		}));
+};
+
+export const getTopDogs = (dogs, compareFunc) => dispatch => {
+	const topDogs = [...dogs].sort(compareFunc).slice(0, 10);
+
+	dispatch({
+		type: GET_TOP_DOGS,
+		payload: topDogs,
+		areTopDogsCalculated: true
+	});
+};
---
+++ src/actions/types.js
@@ -0,0 +1,12 @@
+/*
+ * Created on Sun Mar 08 2020
+ * Author: Gio Justiniano
+ * More info: https://github.com/giomikee/
+ *
+ * Copyright (c) 2020
+ */
+
+export const FETCH_DOG_BREEDS = 'FETCH_DOG_BREEDS';
+export const FETCH_DOG_BREED_IMAGES = 'FETCH_DOG_BREED_IMAGES';
+export const FETCH_ERROR = 'FETCH_ERROR';
+export const GET_TOP_DOGS = 'GET_TOP_DOGS';
