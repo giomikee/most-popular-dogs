b21b5a6743cc55a76a4fa0ca1cbddbda28a01d9e
Gio Justiniano <giojustiniano@Gios-Air.home>
2020-04-10 13:19:50 +0200

    Updates

---
+++ precache-manifest.4e1117cd16d9f679b87f0f805ba220e1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1768fbef7405895b2ea2419ea33e6c8c",
+    "url": "/most-popular-dogs/index.html"
+  },
+  {
+    "revision": "647a921b30e14cfed381",
+    "url": "/most-popular-dogs/static/css/main.77b421c7.chunk.css"
+  },
+  {
+    "revision": "5192942a6944987f6f7d",
+    "url": "/most-popular-dogs/static/js/2.fdc31b76.chunk.js"
+  },
+  {
+    "revision": "eb56b19eba63a53416f4fd778023529f",
+    "url": "/most-popular-dogs/static/js/2.fdc31b76.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "647a921b30e14cfed381",
+    "url": "/most-popular-dogs/static/js/main.7fa247bf.chunk.js"
+  },
+  {
+    "revision": "b2abcc316a6066c50d6f",
+    "url": "/most-popular-dogs/static/js/runtime-main.205640b4.js"
+  }
+]);
